d1e37b23a3e429f64d826776f903c6ceee4501f8
José Miguel Malaca <jose.miguel.malaca@me.com>
2018-05-22 10:48:28 +0100

    add rack::attack to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Tell your app to use the Rack::Attack middleware.
+  config.middleware.use Rack::Attack
 end
